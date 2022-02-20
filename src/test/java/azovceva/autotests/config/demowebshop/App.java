ad713acee72e07a1c37c9a702a7e6b3833514dce
Elizaveta Azovtseva <azovcevae@gmail.com>
2022-02-20 14:09:08 +0700

    delete demoWebShop folders

--- src/test/java/azovceva/autotests/config/demowebshop/App.java
+++
@@ -1,7 +0,0 @@
-package azovceva.autotests.config.demowebshop;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class App {
-    public static azovceva.autotests.config.demowebshop.AppConfig config = ConfigFactory.create(azovceva.autotests.config.demowebshop.AppConfig.class, System.getProperties());
-}
